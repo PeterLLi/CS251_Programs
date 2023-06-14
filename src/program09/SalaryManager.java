a95d5483dda08497322f93ea93995c43382eae7f
peterli <youngenli@gmail.com>
2023-06-14 14:28:36 +0800

    Intend to refactor "employee" codebase. Initial file + constructors.

---
+++ src/program09/SalaryManager.java
@@ -0,0 +1,15 @@
+package program09;
+
+public class SalaryManager {
+    private int id;
+    private double salary;
+    private int yearsOfService;
+
+    public SalaryManager() {
+        this(1, 100000,1);
+    }
+
+    public SalaryManager(int id, double salary, int yearsOfService) {
+
+    }
+}
