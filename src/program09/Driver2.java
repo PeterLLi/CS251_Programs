a11a6d6a008c4daf722227a2efe269e6261a9851
peterli <youngenli@gmail.com>
2023-06-15 17:00:00 +0800

    Added initial tests to second driver.

--- src/program09/Driver2.java
+++ src/program09/Driver2.java
@@ -2,6 +2,13 @@ package program09;
 
 public class Driver2 {
     public static void main(String[] args) {
+        SalaryManager sm = new SalaryManager();
 
+        sm.create("Test1");
+
+        sm.addTo("Test1", "Test2", 1, 100000.00, 4);
+        sm.addTo("Test2", "Test3", 3, 150000.00, 7);
+        sm.addTo("Test3", "Test4", 7, 250000.00, 5);
+        sm.addTo("Test4", "Test5", 11, 300000.00, 11);
     }
 }
