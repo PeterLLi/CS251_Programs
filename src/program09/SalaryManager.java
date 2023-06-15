9ce69b6814268b2693c163ddbc73619d7339a6d8
peterli <youngenli@gmail.com>
2023-06-15 16:30:57 +0800

    Added interface. Added required methods from interface with temp return values.

--- src/program09/SalaryManager.java
+++ src/program09/SalaryManager.java
@@ -4,7 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class SalaryManager {
+public class SalaryManager implements Raiseable{
     private int id;
     private double salary;
     private int yearsOfService;
@@ -53,5 +53,32 @@ public class SalaryManager {
             System.out.println("Failed to create file. Printing stacktrace:");
             e.printStackTrace();
         }
+    }
+
+    public void display(String fileName) {
+
+    }
+
+    public boolean addTo(String inFileName, String outFileName, int id, double salary, int yearsOfService) {
+
+        return false;
+    }
+
+    public boolean removeFrom(String inFileName, String outFileName, int id) {
+
+        return false;
+    }
+
+    public void addService(String inFileName, String outFileName) {
+
+    }
+
+    public int raise(String inFileName, String outFileName, int yearsOfService, double salaryIncPercent) {
+
+        return 0;
+    }
+
+    public void mergeFiles(String inFileName1, String inFileName2, String outFileName) {
+
     }
 }
